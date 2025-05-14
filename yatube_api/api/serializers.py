a0ada3fe772e514913f175a030c4a39d64597e21
Artem Alsimov <arfey163@mail.rum>
2025-05-14 21:07:45 +0400

    12 ошибок

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -16,16 +16,16 @@ class PostSerializer(serializers.ModelSerializer):
         fields = '__all__'
         model = Post
 
-
-
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ['author', 'post']
 
 class FollowSerializer(serializers.ModelSerializer):
     class Meta:
