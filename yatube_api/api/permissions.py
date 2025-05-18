4783b8a73f67da3d5b39d642dc95eccde7301818
Artem Alsimov <arfey163@mail.rum>
2025-05-18 15:58:22 +0400

    15:58

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,12 +5,10 @@ class IsAuthenticatedOrAuthor(permissions.BasePermission):
     """
     Разрешение, которое позволяет редактировать или удалять
      объект только его автору.
-    """ 
+    """
     def has_permission(self, request, view):
-        # Разрешаем безопасные методы для всех
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Для всех остальных методов проверяем аутентификацию
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
