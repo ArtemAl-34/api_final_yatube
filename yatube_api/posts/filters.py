15427d7734b539cf76ae9bf2b32ce3b5b10a5ca0
Artem Alsimov <arfey163@mail.rum>
2025-05-19 16:55:32 +0400

    1

--- yatube_api/posts/filters.py
+++ yatube_api/posts/filters.py
@@ -1,11 +1,11 @@
 from django_filters import rest_framework as filters
-from .models import Follow  # Импортируйте вашу модель
+from .models import Follow
 
 
 class FollowFilter(filters.FilterSet):
     class Meta:
         model = Follow
         fields = {
-            'user': ['exact'],  # Фильтрация по пользователю
-            'following': ['exact'],  # Фильтрация по подпискам
+            'user': ['exact'],
+            'following': ['exact'],
         }
